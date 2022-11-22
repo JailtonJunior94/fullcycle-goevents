504304673db00ef6376ffbce723d4e1180fb2f64
Jailton Junior <jailton.junior94@outlook.com>
2022-11-22 16:17:09 -0300

    Conclusão do capitulo: Manipulando eventos

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/jailtonjunior94/fullcycle-goevents/pkg/rabbitmq"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+
+	messages := make(chan amqp.Delivery)
+	go rabbitmq.Consumer(ch, messages, "minhafila")
+
+	for message := range messages {
+		fmt.Println(string(message.Body))
+		message.Ack(false)
+	}
 }
--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/jailtonjunior94/fullcycle-goevents/pkg/rabbitmq"
+)
+
+func main() {
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+
+	err = rabbitmq.Publish(ch, "Hello World!", "amq.direct")
+	if err != nil {
+		fmt.Println(err)
+	}
 }
